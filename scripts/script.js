8e13f40a0134a9f4933b676f302ef9456d2ee10a
Manuel Eugenio Losada Clua <ManuelEugenio.LosadaClua@endava.com>
2022-07-16 20:20:40 -0300

    Creacion de la funcion para limpiar el formulario

--- scripts/script.js
+++ scripts/script.js
@@ -25,6 +25,7 @@ window.onload = () => {
             modalpName.innerText = nombre.value.concat(" ", apellido.value); //envia nombre y apellido registrados al modal
             modal.classList.add("modal-show"); //muestra el modal
             modalClose.onclick = function(){ //cuando se presiona el boton cerrar del modal
+                limpiarFormulario();
                 modal.classList.remove("modal-show"); //lo cierra
             }
             window.onclick = function(e) { //cuando se presiona cualquier parte fuera del modal
@@ -75,4 +76,8 @@ function ocultarLabels(){
             textList[i].labels[1].classList.toggle("hidden",true)
         }
     }
+}
+
+function limpiarFormulario() {
+    document.getElementById("miForm").reset();
 }
